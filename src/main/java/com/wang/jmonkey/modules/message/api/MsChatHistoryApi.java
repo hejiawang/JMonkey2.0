ff17ce6d0258b34077790480a5f511c69853bae8
hejiawang <952327407@qq.com>
2019-02-24 11:12:02 +0800

    init

--- src/main/java/com/wang/jmonkey/modules/message/api/MsChatHistoryApi.java
+++ src/main/java/com/wang/jmonkey/modules/message/api/MsChatHistoryApi.java
@@ -4,14 +4,12 @@ import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.plugins.Page;
 import com.wang.jmonkey.common.http.abs.BaseHttp;
 import com.wang.jmonkey.common.http.result.HttpPageResult;
-import com.wang.jmonkey.common.http.result.HttpResult;
 import com.wang.jmonkey.modules.message.model.entity.MsChatHistory;
 import com.wang.jmonkey.modules.message.service.IMsChatHistoryService;
 
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.io.Serializable;
 
 /**
  * @Description: 消息聊天记录 api
@@ -36,46 +34,6 @@ public class MsChatHistoryApi extends BaseHttp {
         EntityWrapper wrapper = new EntityWrapper<MsChatHistory>();
 
         return new HttpPageResult<>( service.selectPage( page, wrapper ) );
-    }
-
-    /**
-     * 保存实体信息
-     * @param entity 实体信息
-     * @return
-     */
-    @PostMapping(value = "/save")
-    public HttpResult<Boolean> save( @RequestBody MsChatHistory entity ){
-        return new HttpResult<>(service.insert(entity));
-    }
-
-    /**
-     * 修改实体信息
-     * @param entity 实体信息
-     * @return
-     */
-    @PutMapping(value = "/modify")
-    public HttpResult<Boolean> modify( @RequestBody MsChatHistory entity ){
-        return new HttpResult<>(service.updateById(entity));
-    }
-
-    /**
-     * 删除实体信息
-     * @param id 实体ID
-     * @return
-     */
-    @DeleteMapping(value = "/delete/{id}")
-    public HttpResult<Boolean> delete( @PathVariable Serializable id ){
-        return new HttpResult<>(service.deleteById(id));
-    }
-
-    /**
-     * 查找实体信息
-     * @param id 实体ID
-     * @return
-     */
-    @GetMapping(value = "/find/{id}")
-    public HttpResult<MsChatHistory> findById(@PathVariable Serializable id ){
-        return new HttpResult<>(service.selectById(id));
     }
 
 }
--- src/main/java/com/wang/jmonkey/modules/message/task/MsChatClearHistoryTask.java
+++ src/main/java/com/wang/jmonkey/modules/message/task/MsChatClearHistoryTask.java
@@ -1,19 +1,42 @@
 package com.wang.jmonkey.modules.message.task;
 
 import com.wang.jmonkey.common.quartz.JMonkeyBaseTask;
+import com.xiaoleilu.hutool.date.DateUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 
 /**
- * @Description: 清空聊天记录
+ * @Description: 清空聊天记录, 并将清空的聊天记录保存到一个excel文件中
  * @Auther: HeJiawang
  * @Date: 2019-02-24
  */
+@Slf4j
+@Component
 public class MsChatClearHistoryTask extends JMonkeyBaseTask {
+
+    /**
+     * 自动删除n天前的聊天记录
+     */
+    @Value("${jmonkey.message.chat.history.clear}")
+    private int clear;
+
+    /**
+     * 删除的聊天记录是否保存到Excel文件中
+     */
+    @Value("${jmonkey.message.chat.history.backups}")
+    private boolean backups;
 
     /**
      * 任务执行
      */
     @Override
     public void run() {
+        String clearDate = DateUtil.offsetDay(new Date(), clear).toString("yyyy-MM-dd");
+        log.debug(clearDate);
+        log.debug(String.valueOf(backups));
     }
 }
